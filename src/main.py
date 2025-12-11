b50f24cf067fb2a8a652e3141574d4155e50a914
Félix Mielcarek <felix.mielcarek10@gmail.com>
2025-12-11 09:53:18 +0100

    Move main to src

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
-# début code branch Maps
+#region IMPORTS
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+#endregion
 
-url =r"..\Data\Donnees-sur-le-parc-de-vehicules-au-niveau-communal.2025-09.csv" #"https://data.statistiques.developpement-durable.gouv.fr/dido/api/v1/datafiles/1582861b-e042-4490-9161-6429d8229703/csv?COMMUNE_CODE=startsWith%3A77"
 
-df = pd.read_csv(url,delimiter=';',encoding='utf-8')
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP,"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"])#Création du site en precisant qu'on ne veut pas de message d'erreur avec " suppress_callback_exceptions=True"
+#region APP CONFIG
+app = dash.Dash(
+    __name__, 
+    external_stylesheets=[dbc.themes.BOOTSTRAP,"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"]
+) 
 app.title="Parc automobile Francais"
+
+server = app.server
+app.config.suppress_callback_exceptions = True
+#endregion
+
+#region READ DATA
+df = pd.read_csv("../data/raw/rawdata.csv",delimiter=';',encoding='utf-8')
+#endregion
+
+
+app.layout = app_layout
+
 app.index_string=f"""
     <!DOCTYPE html>
         <html>
@@ -26,7 +43,6 @@ app.index_string=f"""
             </body>
         </html>
 """
-app.layout = app_layout
 
 
 if __name__ == '__main__':
